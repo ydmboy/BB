56e72b3c517b52f9ff2d4a53d5eb8e0d43913fe2
ydmboy <3634076145@qq.com>
2024-10-15 18:31:58 +0800

    initial bb

---
+++ Blackbone/TestDriver/TestDriver.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestDriver
+{
+	TEST_CLASS(TestDriver)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
